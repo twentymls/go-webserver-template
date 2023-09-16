afe787c3959fe1014b03b3f397044f60acec31b7
Pietro Ventimiglia <50101372+twentymls@users.noreply.github.com>
2023-09-16 12:02:22 +0200

    setup env loading + port initalization

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/twentymls/go-server-test
+
+go 1.21.1
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT not found!")
+	}
+
+	fmt.Println("Server running on port", port)
+}
